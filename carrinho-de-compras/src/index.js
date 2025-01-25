82f0bfb2f7b2f97d1974e0bbb38ecb8bc491edbf
jesse <nitchsx@gmail.com>
2025-01-25 12:34:36 -0300

    feat: complet project

--- carrinho-de-compras/src/index.js
+++ carrinho-de-compras/src/index.js
@@ -1,1 +1,25 @@
-console.log("hello");
+import createItem from "./services/item.js";
+import * as cartService from "./services/cart.js";
+
+const myCart = [];
+const myWhishList = [];
+
+console.log("Welcome to the your shopee Cart!");
+
+const item1 = await createItem("hotwheels ferrari", 20.99, 1);
+const item2 = await createItem("hotwheels lamborguini", 29.99, 3);
+
+//adicionar
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+await cartService.removeItem(myCart, 1);
+
+await cartService.displayCart(myCart);
+
+// console.log(item2.subtotal());
+//deletar item
+// await cartService.deleteItem(myCart, item2.name);
+// await cartService.deleteItem(myCart, item1.name);
+
+await cartService.calculateTotal(myCart);
--- carrinho-de-compras/src/services/cart.js
+++ carrinho-de-compras/src/services/cart.js
@@ -1,11 +1,42 @@
-// adicionar item , assinatura de metodo
-async function addItem(cart) {}
+// adicionar item , assinatura de metodo ✅
+async function addItem(userCart, item) {
+  userCart.push(item);
+}
 
 //deleter item
-async function deleteItem(userCart, name) {}
+async function deleteItem(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
+
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
+}
 
 //remover
-async function removeItem(userCart, index) {}
+async function removeItem(userCart, index) {
+  const deleteIndex = index - 1;
+
+  if (index >= 0 && index < userCart.length) {
+    userCart.splice(deleteIndex, 1);
+  }
+}
 
 // calcular total
-async function calculateTotal(userCart) {}
+async function calculateTotal(userCart) {
+  console.log("\nShopee cart total is: ");
+  const result = userCart.reduce((total, item) => total + item.subtotal(), 0);
+  console.log(`Total🛒 : ${result}`);
+}
+
+async function displayCart(userCart) {
+  console.log("\nShopee cart list:");
+  userCart.forEach((item, index) => {
+    console.log(
+      `${index + 1}. ${item.name}. R$ - ${item.price}. | ${
+        item.quantity
+      }x | Subtotal = ${item.subtotal()}`
+    );
+  });
+}
+
+export { addItem, deleteItem, removeItem, calculateTotal, displayCart };
