25bb45c7051bba857c6be56965d42a3d16cde685
jesse <nitchsx@gmail.com>
2025-02-23 02:06:00 -0300

    feat: complet

--- criandoQrCode/src/index.js
+++ criandoQrCode/src/index.js
@@ -1,13 +1,13 @@
 import prompt from 'prompt';
 import mainPrompot from './prompts/prompt-main.js';
 import chalk from 'chalk';
+import createQrcode from '../services/qr-code/create.js';
+import createPassword from './password/create.js';
 
 async function main() {
     prompt.get(mainPrompot, async (err, choose) => {
-        if (choose.select == 1) {console.log(chalk.bgGreen("Escolheu QRCODE"));
-         }
-        if (choose.select == 2) { console.log(chalk.bgGreen("Escolheu PASSWORD"));
-         }
+        if (choose.select == 1) {await createQrcode();}
+        if (choose.select == 2) {await createPassword();}
     });
     prompt.start();
 }
