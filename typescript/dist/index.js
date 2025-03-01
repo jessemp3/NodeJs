56af322a2d155961239405dad1f2a58d4293b1fe
jesse <nitchsx@gmail.com>
2025-03-01 20:24:21 -0300

    feat: classss

---
+++ typescript/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function main() {
+    let a = "Hello World";
+    console.log(a);
+    const name = "jesse";
+    const age = 20;
+    console.log(`My name is ${name} and I am ${age} years old`);
+}
+main();
---
+++ typescript/src/index.ts
@@ -0,0 +1,11 @@
+function main(){
+    let a:string = "Hello World";
+    console.log(a);
+
+    const name : string = "jesse"
+    const age : number = 20
+
+    console.log(`My name is ${name} and I am ${age} years old`)
+}
+
+main()
